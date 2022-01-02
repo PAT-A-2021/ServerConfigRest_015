b0bddbee03ad64756f4ff599130ff2846c505c50
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2022-01-02 09:24:54 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceRest_20190140015_Muhamad_Arief_P_Suradi;
+using System.ServiceModel;
+
+namespace ServerConfigRest_20190140015_Muhamad_Arief_P_Suradi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+
+            try
+            {
+                hostObj = new ServiceHost(typeof(TI_UMY));
+                hostObj.Open();
+                Console.WriteLine("Servicenya nyala ges...");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
